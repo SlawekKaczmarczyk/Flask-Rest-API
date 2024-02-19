9ea2d6f1c45b9ade047b7c4e433151ef6d3ead61
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2024-02-19 13:14:44 +0100

    create and read methods

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required = True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required = True)
+video_put_args.add_argument("rating", type=int, help="Rating of the video out of 10", required = True)
+ 
+videos={}
 
-class Hello(Resource):
-    def get(self):
-        return {"data": "Hello World"} # return a json format
-    def post(self):
-        return {"data": "Hello Posted"} 
+
+class Video(Resource):
+    def get(self,video_id):
+        return videos[video_id] # return a json format
     
+    def put(self,video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args 
+        return videos[video_id], 201 #201 stands for created
     
-api.add_resource(Hello, "/hello")
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
